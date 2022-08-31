89c2c9f33aa6ec1805dc6223373394ecbb398f17
ridiculousigit <jsigit243@gmail.com>
2022-08-31 19:22:40 +0700

    Initial commit

--- app/src/main/java/binar/academy/chapter3topic1/BMIActivity.kt
+++ app/src/main/java/binar/academy/chapter3topic1/BMIActivity.kt
@@ -1,35 +1,47 @@
 package binar.academy.chapter3topic1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_bmi.*
+import kotlinx.android.synthetic.main.activity_bmi.view.*
 
 class BMIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi)
 
-        btnHitung.setOnClickListener {
+        // Tombol Kembali ke MainActivity
+        goHome_BMI.setOnClickListener {
+            var menu = Intent(this, MainActivity :: class.java)
+            startActivity(menu)
+        }
+
+        // Tombol Hitung BMI
+        btn_hitungBMI.setOnClickListener {
 
             var tinggiBadan = editTB.text.toString()
             var beratBadan = editBB.text.toString()
             var BMI = hitungBMI(tinggiBadan.toDouble(), beratBadan.toDouble())
 
-            textHasilUmur.text = "${editUmur.text} tahun"
-            textHasilTB.text = "$tinggiBadan cm"
-            textHasilBB.text = "$beratBadan kg"
-            textHasilBMI.text = String.format("%.1f", BMI)
-            textHasilKategori.text = kategoriBerat(BMI)
+            hasilUmur.text = "${editUmur.text} tahun"
+            hasilTB.text = "$tinggiBadan cm"
+            hasilBB.text = "$beratBadan kg"
+            hasilBMI.text = String.format("%.1f", BMI)
+            hasilKategori.text = kategoriBerat(BMI)
 
         }
 
-        btnReset.setOnClickListener {
+        // Tombol Reset BMI
+        btn_resetBMI.setOnClickListener {
 
             reset()
+
         }
 
     }
 
+    // Method Perhitungan BMI
     fun hitungBMI(tinggiTubuh : Double, beratTubuh : Double) : Double {
 
         var BMI = beratTubuh / ((tinggiTubuh * tinggiTubuh) / 10000)
@@ -37,6 +49,7 @@ class BMIActivity : AppCompatActivity() {
         return BMI
     }
 
+    // Method Klasifikasi BMI
     fun kategoriBerat(BMI: Double):String {
 
         val kategori: String
@@ -61,15 +74,16 @@ class BMIActivity : AppCompatActivity() {
         return kategori
     }
 
+    // Method untuk Tombol Reset BMI
     fun reset(){
         editUmur.setText("")
         editTB.setText("")
         editBB.setText("")
 
-        textPreUmur.text = ""
-        textPreTB.text = ""
-        textPreBB.text = ""
-        textPreBMI.text = ""
-        textPreKategori.text = ""
+        hasilUmur.text = ""
+        hasilTB.text = ""
+        hasilBB.text = ""
+        hasilBMI.text = ""
+        hasilKategori.text = ""
     }
 }
--- app/src/main/java/binar/academy/chapter3topic1/NilaiActivity.kt
+++ app/src/main/java/binar/academy/chapter3topic1/NilaiActivity.kt
@@ -1,11 +1,78 @@
 package binar.academy.chapter3topic1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_bmi.*
+import kotlinx.android.synthetic.main.activity_nilai.*
 
 class NilaiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nilai)
+
+        // Tombol Kembali ke MainActivity
+        goHome_Nilai.setOnClickListener {
+
+            var menu = Intent(this, MainActivity :: class.java)
+            startActivity(menu)
+
+        }
+
+        btn_hitungNilai.setOnClickListener {
+
+            var namaMahasiswa = editNama.text.toString()
+            var nimMahasiswa = editNIM.text.toString()
+            var uasMahasiswa = editUAS.text.toString()
+            var utsMahasiswa = editUTS.text.toString()
+            var tugasMahasiswa = editTugas.text.toString()
+            var total = hitungTotal(uasMahasiswa.toDouble(), utsMahasiswa.toDouble(), tugasMahasiswa.toDouble())
+
+            hasilNama.text = "${editNama.text}"
+            hasilNIM.text = "${editNIM.text}"
+            hasilUAS.text = "${editUAS.text}"
+            hasilUTS.text = "${editUTS.text}"
+            hasilTugas.text = "${editTugas.text}"
+            hasilTotal.text = String.format("%.1f", total)
+        }
+
+        btn_resetNilai.setOnClickListener {
+
+            reset()
+
+        }
+
+    }
+
+    fun hitungTotal(UAS : Double, UTS : Double, Tugas : Double) : Double {
+
+        var hasilTotal = UAS + UTS + Tugas
+
+        return hasilTotal
+
+    }
+
+
+
+    fun reset() {
+
+        editNama.setText("")
+        editNIM.setText("")
+        editUAS.setText("")
+        editUTS.setText("")
+        editUAS.setText("")
+        editTugas.setText("")
+
+        hasilNama.text = ""
+        hasilNIM.text = ""
+        hasilUAS.text = ""
+        hasilUTS.text = ""
+        hasilUTS.text = ""
+        hasilTugas.text = ""
+        hasilTotal.text = ""
+        hasilRerata.text = ""
+        hasilKriteria.text = ""
+        hasilStatus.text = ""
+
     }
 }
